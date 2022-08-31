71a1d184f2dea3a3354467139ca868d80d9fd1da
Elian0019 <elianalvarez80@gmail.com>
2022-08-31 14:52:29 -0400

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Clientes')
+
+@section('content_header')
+    <h1>Clientes</h1>
+@stop
+
+@section('content')
+    <p><h1>Aqui se van a registrar los clientes prra.</h1></p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
